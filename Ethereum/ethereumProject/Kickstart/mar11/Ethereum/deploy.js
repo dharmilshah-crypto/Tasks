d8364a48741c2ed7b177c8840d36222f01b26380
dharmilshah-crypto <dharmil.shah@solulab.co>
2021-03-11 18:44:37 +0530

    mar11

---
+++ Ethereum/ethereumProject/Kickstart/mar11/Ethereum/deploy.js
@@ -0,0 +1,24 @@
+const HDWalletProvider = require("truffle-hdwallet-provider");
+const Web3 = require("web3");
+// const { interface, bytecode } = require("./compile");
+const compiledFactory = require('./build/CampaignFactory.json')
+
+const provider = new HDWalletProvider(
+  "stand drill mobile mean tent whip horror ritual truth lawn version alarm",
+  "https://rinkeby.infura.io/v3/44274788c5da4c38ac096f248d577767"
+);
+const web3 = new Web3(provider);
+
+const deploys = async () => {
+  const accounts = await web3.eth.getAccounts();
+  //   console.log(JSON.parse(interface));
+  console.log("Attempting to deply from account ", accounts[0]);
+  const result = await new web3.eth.Contract(JSON.parse(compiledFactory.interface))
+    .deploy({
+      data: compiledFactory.bytecode
+    })
+    .send({ gas: "2000000", from: accounts[0] });
+    // console.log(interface)
+  console.log("contract deployed to", result.options.address);
+};
+deploys();
